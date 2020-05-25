40b042fafe71684f678c1047011d8beea5eab9c2
Jonas Hecht <jonas.hecht@codecentric.de>
2020-05-25 09:04:57 +0200

    Now closing the connection finally every time. Therefore our H2 in-memory database is persistent in one JVM, not only connection-wise. Also using Logger for Exception logging.

--- 02-jdbc/README.md
+++ 02-jdbc/README.md
@@ -1,5 +1,5 @@
 # java2-examples: 02-jdbc
-Example code showing how to work with H2 (embedded) database.
+Example code showing how to work with in-memory H2 database.
 
 See the docs here: http://www.h2database.com/html/features.html#in_memory_databases
 
--- 02-jdbc/src/main/java/io/jonashackt/lectures/exercises/storage/AddressbookRepository.java
+++ 02-jdbc/src/main/java/io/jonashackt/lectures/exercises/storage/AddressbookRepository.java
@@ -2,6 +2,8 @@ package io.jonashackt.lectures.exercises.storage;
 
 import io.jonashackt.lectures.exercises.Person;
 import org.h2.jdbcx.JdbcDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,20 +15,19 @@ import java.sql.Statement;
 
 public class AddressbookRepository {
 
-    private Connection connection;
+    private static final Logger LOG = LoggerFactory.getLogger(AddressbookRepository.class);
+    private JdbcDataSource dataSource;
     private String databaseName = "addressbook";
 
     public AddressbookRepository() throws StorageError {
-
-        try {
-            //Same as Connection connection = DriverManager.getConnection("jdbc:h2:mem:" + databaseName);
-            JdbcDataSource dataSource = new JdbcDataSource();
-            dataSource.setURL("jdbc:h2:mem:" + databaseName);
-            connection = dataSource.getConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new StorageError(e.getMessage());
-        }
+        dataSource = new JdbcDataSource();
+        // see http://www.h2database.com/html/features.html#in_memory_databases
+        // because we want to persist our in-memory H2 database as long as our JVM is alive (or tests run)
+        // we need to add ;DB_CLOSE_DELAY=-1 here. With that we also have the same behaviour like a 'real' DB
+        // and can use multiple JDBC connections with dataSource.getConnection()
+        // otherwise every connection would also get a new in-memory database, without the pre-created tables
+        // from other connections
+        dataSource.setURL("jdbc:h2:mem:" + databaseName + ";DB_CLOSE_DELAY=-1");
     }
 
     public void createTablePerson() throws StorageError {
@@ -40,9 +41,13 @@ public class AddressbookRepository {
     }
 
     public Person readPerson() throws StorageError {
+
+        Connection connection = null;
         try {
+            connection = dataSource.getConnection();
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select * from PERSON");
+
             String firstName = "";
             String lastName = "";
             String eMail = "";
@@ -54,19 +59,36 @@ public class AddressbookRepository {
             return new Person(firstName, lastName, eMail);
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOG.error(e.getMessage());
             throw new StorageError(e.getMessage());
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                LOG.error(e.getMessage());
+                throw new StorageError(e.getMessage());
+            }
         }
     }
 
     private void executeStatement(String sqlStatement) throws StorageError {
+
+        Connection connection = null;
         try {
+            connection = dataSource.getConnection();
             Statement statement = connection.createStatement();
             statement.execute(sqlStatement);
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOG.error(e.getMessage());
             throw new StorageError(e.getMessage());
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                LOG.error(e.getMessage());
+                throw new StorageError(e.getMessage());
+            }
         }
     }
 
@@ -81,7 +103,7 @@ public class AddressbookRepository {
         try {
             return Files.readString(Paths.get("src/main/resources/" + fileName));
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.error(e.getMessage());
             throw new StorageError(e.getMessage());
         }
     }
