04503a8534f5eff474b22806ccd905ef423d2b5c
Jonas Hecht <jonas.hecht@codecentric.de>
2020-06-21 16:14:41 +0200

    Add saveAndRetrieveTest with rest-assured

--- 06-spring-rest-jpa/src/test/java/io/jonashackt/lectures/exercises/controller/AddressBookControllerTest.java
+++ 06-spring-rest-jpa/src/test/java/io/jonashackt/lectures/exercises/controller/AddressBookControllerTest.java
@@ -1,9 +1,11 @@
 package io.jonashackt.lectures.exercises.controller;
 
+import io.jonashackt.lectures.exercises.domain.Person;
 import io.restassured.RestAssured;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
@@ -17,6 +19,8 @@ public class AddressBookControllerTest {
 
     @LocalServerPort
     private int port;
+
+    @Autowired AddressBookController addressBookController;
 
     @BeforeEach
     public void init() {
@@ -32,6 +36,24 @@ public class AddressBookControllerTest {
             .statusCode(HttpStatus.SC_OK)
             .assertThat()
                 .body(is(equalTo(AddressBookController.HEALTH_RESPONSE)));
+    }
+
+    @Test
+    public void isLuisSavedAndRetrieved() {
+
+        // Table Person is automatically created by JPA Persistence Provider
+        Person luis = new Person("Luis", "Meyer", "luis.meyer@web.de");
+
+        Person isThisLuis = when()
+                                .post("/person/Meyer/Luis/luis.meyer@web.de")
+                            .then()
+                                .statusCode(HttpStatus.SC_CREATED)
+                                .extract()
+                                    .body().as(Person.class);
+
+        assertEquals("Luis", isThisLuis.getFirstName());
+        assertEquals("Meyer", isThisLuis.getLastName());
+        assertEquals("luis.meyer@web.de",isThisLuis.geteMail());
     }
 
 }
